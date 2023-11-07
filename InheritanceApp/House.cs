792cc38a0a53dcc48dfee29bb9d51e6772f5e02d
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-07 10:36:47 +0100

    Add project files.

---
+++ InheritanceApp/House.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApp
+{
+	internal class House
+	{
+		public int SquareMeters { get; private set; }
+		public int Year { get; set; }
+		public string Color { get; set; }
+		public int MonthlyPrice { get; private set; }
+		public House(int squareMeters, int year = 0, string color = "Unknown Color") 
+		{
+			SquareMeters = squareMeters;
+			Year = year;
+			Color = color;
+		}
+
+		public int GetSquareMeters()
+		{
+			return SquareMeters;
+		}
+
+		public void SetSquareMeters(int squareMeters)
+		{
+			if (SquareMeters == 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(squareMeters), squareMeters, "The house cannot be 0 squaremeters");
+			}
+			else
+			{
+				SquareMeters = squareMeters;
+			}	
+		}
+
+		public int GetPricePerSqrMeters(int price)
+		{
+			int monthlySum = price / 12;
+			return monthlySum / SquareMeters;
+		}
+	}
+}
---
+++ InheritanceApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InheritanceApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			House tinyHouse = new(50);
+            Console.WriteLine(tinyHouse.GetPricePerSqrMeters(120000));
+        }
+	}
+}
